2f8dfde116179cbfa9de45b148b3a3a442a5971f
gvasko <gabor.vaskoszedlar@gmail.com>
2026-04-08 16:07:18 +0200

    fix logs and inc rsync rate

--- nvr/init/init-cam-backup-log.sh
+++ nvr/init/init-cam-backup-log.sh
@@ -3,5 +3,6 @@
 cp ./cam-backup /etc/logrotate.d/cam-backup
 touch /var/log/cam-backup.log
 logrotate -f /etc/logrotate.d/cam-backup
+chown root:adm /var/log/cam-backup.log
 chmod 664 /var/log/cam-backup.log
 
--- nvr/init/init-cam-upload-log.sh
+++ nvr/init/init-cam-upload-log.sh
@@ -3,5 +3,6 @@
 cp ./cam-upload /etc/logrotate.d/cam-upload
 touch /var/log/cam-upload.log
 logrotate -f /etc/logrotate.d/cam-upload
+chown root:adm /var/log/cam-upload.log
 chmod 664 /var/log/cam-upload.log
 
